f8d647a66c49719a08ba39a8b5b063abe3561e62
Gero Zayas <gerozayas@gmail.com>
2024-10-18 19:45:35 +0200

    Add exercise

--- Program.cs
+++ Program.cs
@@ -119,3 +119,43 @@ class Program
 
 */
 
+/*
+9. Write a C# Sharp program that takes four numbers as input to calculate and print the average.
+Test Data:
+Enter the First number: 10
+Enter the Second number: 15
+Enter the third number: 20
+Enter the four number: 30
+
+Expected Output:
+The average of 10 , 15 , 20 , 30 is: 18
+*/
+
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This program takes 4 numbers and gives their average");
+
+        Console.WriteLine("Insert first nummber: ");
+        int first = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insert second nummber: ");
+        int second = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insert third nummber: ");
+        int third = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insert fourth nummber: ");
+        int fourth = Convert.ToInt32(Console.ReadLine());
+
+        int avg = (first + second + third + fourth) / 4;
+
+        Console.WriteLine($"The average of these numbers is: {avg}");
+    }
+}
+
+*/
+
