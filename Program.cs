8700fc518f70f62f7ff3b891550f94c1ad0af91e
Gero Zayas <gerozayas@gmail.com>
2024-10-18 17:11:07 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*Exercises from https://www.w3resource.com/csharp-exercises/basic/index.php*/
+
+using System;
+
+/*
+Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
+Test Data:
+Input the first number to multiply: 2
+Input the second number to multiply: 3
+Input the third number to multiply: 6
+Expected Output:
+2 x 3 x 6 = 36
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
